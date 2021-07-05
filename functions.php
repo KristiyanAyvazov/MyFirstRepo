4f65d1c80ad2b6f14962aabc6f5521a1fdd24dbb
Kristiyan Ayvazov <kristiyan.ayvazov@Kristiyan-Ayvazov.local>
2021-07-05 11:01:40 +0300

    loops 2nd attempt

--- functions.php
+++ functions.php
@@ -1,4 +1,54 @@
 <?php
+// Exercise 1.
 function learning_functions( $string ) {
 	echo $string;
+}
+
+// Exercise 2.
+$bool === false;
+function boolean_checker( $bool ) {
+	if ( $bool === true ) {
+		echo 'Foo';
+		echo "\n";
+	} else {
+		echo 'Bar';
+		echo "\n";
+	}
+}
+
+// Exercise 3.
+$i = 0;
+while ( $i <= 100) {
+	if ( $i % 3 == 0 ) {
+		echo 'Foo';
+		echo "\n";
+	} 
+
+	if ( $i % 5 == 0 ) {
+		echo 'Bar';
+		echo "\n";
+	}
+
+	if ( $i % 3 == 0 && $i % 5 == 0 ) {
+		echo 'FooBar';
+		echo "\n";
+	}
+	$i++;
+}
+
+foreach ( range( 0,12 ) as $key) {
+	if ( $key % 3 == 0 ) {
+		echo 'Foo';
+		echo "\n";
+	} 
+
+	if ( $key % 5 == 0 ) {
+		echo 'Bar';
+		echo "\n";
+	}
+
+	if ( $key % 3 == 0 && $i % 5 == 0 ) {
+		echo 'FooBar';
+		echo "\n";
+	}
 }
